b284a561827bc8295ac3bdba7f02796ba4801d58
Maxim Dobrovolsky <dobrovolsky.m.ya@gmail.com>
2021-06-17 14:32:03 +0300

    fix genre

--- catalog/admin.py
+++ catalog/admin.py
@@ -24,7 +24,7 @@ admin.site.register(Author, AuthorAdmin)
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'display_genre')
+    list_display = ('title', 'author')
 
     def display_genre(self):
         """
@@ -40,10 +40,12 @@ class BookAdmin(admin.ModelAdmin):
 @admin.register(BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+
     fieldsets = (
         (None, {
             'fields': ('book', 'imprint', 'id')
         }),
         ('Availability', {
             'fields': ('status', 'due_back')
-        }),)
+        }),
+    )
